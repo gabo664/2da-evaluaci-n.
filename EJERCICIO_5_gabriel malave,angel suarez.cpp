75d1b8db632d8e7c26f9ba0d288b14d811ba1107
gabo664 <elgabo16082007@gmail.com>
2025-10-22 16:41:45 -0400

    Add files via upload

---
+++ EJERCICIO_5_gabriel malave,angel suarez.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int rango;
+
+	cout<<"ingrese el rango de numeros primos"<<endl;
+	cin>>rango;
+	for (int x=2;x<=rango;x++){
+	     x;
+		int primo=1;
+	
+
+	for(int i=2; i * i<=x;i++){
+	if (i%x==0){
+		primo=0;
+		
+	}
+	
+}
+	 if (i==1){
+	 
+	 cout<<x<<endl;
+}
+}
+return 0;
+}
